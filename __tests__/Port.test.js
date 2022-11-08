ba16d243957d762089efac1363b3eac19e8f48dc
James Goodman <jamesgoodman206@gmail.com>
2022-11-08 20:38:29 +0000

    refactored the port tests, included beforeEach on them instead of repeating myself

--- __tests__/Port.test.js
+++ __tests__/Port.test.js
@@ -1,14 +1,22 @@
 const Port = require('../src/Port.js');
 
 describe('Port', () => {
+  describe('with port',() => {
+
+  let port;
+
+  beforeEach (() => {
+    port = new Port('Dover')
+  });
+  
     it('can be instantiated', () => {
       expect(new Port()).toBeInstanceOf(Object);
     });
     it('port has a name', () => {
-        const port = new Port('Dover');
+        expect(port.name).toEqual('Dover')
     });
     it('can add a ship', () => {
-      const port = new Port('Dover');
+      
       const ship = {};
 
       port.addShip(ship);
@@ -16,7 +24,7 @@ describe('Port', () => {
       expect(port.ships).toContain(ship);
     });
     it('can remove a ship', () => {
-      const port = new Port('Dover');
+      
       const titanic = {};
       const queenMary = {};
 
@@ -27,3 +35,4 @@ describe('Port', () => {
       expect(port.ships).toEqual([titanic]);
     });
   });
+});
